a0d7d16870d39f81437c6a25be42e154e775fa2a
kerenLaor <keren199622@gmail.com>
2024-03-04 07:06:27 +0200

    format

--- src/main/java/com/client/demo/controllers/DemoController.java
+++ src/main/java/com/client/demo/controllers/DemoController.java
@@ -1,9 +1,7 @@
 package com.client.demo.controllers;
 
-import com.client.demo.models.WebHookRequest;
 import com.schemas.modelRest.TimerWebhookResponse;
 import com.schemas.modelRest.WebhookTimerRequest;
-import com.schemas.timerRest.ApiRest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,12 +16,4 @@ public class DemoController implements com.schemas.timerRest.ApiRest {
         return new ResponseEntity<>(new TimerWebhookResponse().message("webhook created"),
                 HttpStatus.OK);
     }
-
-    /*Logger logger = LoggerFactory.getLogger(DemoController.class);
-
-    @PostMapping("/api/timers/outpost")
-    public ResponseEntity<String> postWebhook(@RequestBody WebHookRequest payload) {
-        logger.warn("Webhook was triggered {}", payload.getMessage());
-        return new ResponseEntity<>("Webhook received successfully", HttpStatus.OK);
-    }*/
 }
